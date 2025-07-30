dcac4b7f4dee04d23a7201d9ea538c2386ce9597
c0rs3 <you@example.com>
2025-07-31 00:19:33 +0300

    ImGui headers

--- headers/raycasterutil.h
+++ headers/raycasterutil.h
@@ -1,9 +1,14 @@
 #ifndef RAYCASTER_UTIL
 #define RAYCASTER_UTIL  
+
+// Flags
+#define USE_MAP_EDITOR
+
 // STL
 #include <cmath>
 #include <iostream>
 #include <stdlib.h>
+#include <stdio.h>
 #include <vector>
 #include <stdint.h>
 #include <filesystem>
@@ -11,6 +16,7 @@
 #include <sys/stat.h>
 
 // SDL 
+#include <SDL3/SDL.h>
 #include "SDL3/SDL_events.h"
 #include "SDL3/SDL_init.h"
 #include "SDL3/SDL_render.h"
@@ -19,9 +25,13 @@
 #include "SDL3/SDL_video.h"
 #include "SDL3/SDL_keyboard.h"
 #include "SDL3/SDL_opengl.h"
-// IMGUI
-#include "imgui.h"
-#include "imgui_impl_sdl3.h"
-#include "imgui_impl_opengl3.h"
+
+#ifdef USE_MAP_EDITOR
+#include <imgui.h>
+#include <imgui_impl_sdl3.h>
+#include <imgui_impl_sdlrenderer3.h>
+#endif
+
+
 
 #endif
